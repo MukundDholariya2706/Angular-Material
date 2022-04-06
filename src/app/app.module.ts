d6cd9f13ae6bd070358016b192d9c65843d83e2b
Mukund <mukunddtridhyatech@gmail.com>
2022-04-06 16:15:59 +0530

    material button-toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { ButtonComponent } from './material/button/button/button.component';
-import { HeadingComponent } from './material/typography/heading/heading.component';
+import { ButtonComponent } from './material/button/button.component';
+import { HeadingComponent } from './material/typography/heading.component';
 import { MaterialModule } from './material/material.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonToggleComponent } from './material/button-toggle/button-toggle.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadingComponent,
-    ButtonComponent
+    ButtonComponent,
+    ButtonToggleComponent,
   ],
   imports: [
     BrowserModule,
